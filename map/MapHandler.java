8bc9d02ed2d37786dc73b1bdfbc1dbf07c822079
lpinilla <lpinilla@users.noreply.github.com>
2017-06-17 00:26:03 -0300

    Update MapHandler.java
    Agregadas validaciones, quitado métodos createMultipleMap y changeMap y agregadas nuevas excepciones puestas ahora en Runtime y después a cambiar

--- map/MapHandler.java
+++ map/MapHandler.java
@@ -1,13 +1,11 @@
 package map;
 
-
 import java.util.HashMap;
 import java.util.ArrayList;
 
-
-/*Objeto que ser encarga de crear, cambiar y
- * modificar (o indicar que tienen que modificar) -> se pueden modificar?
- * los mapas
+/*Objeto que ser encarga de crear, cambiar
+ *y devolver los mapas para que puedan ser
+ *utilzados por el juego
  */
 
 /**
@@ -22,28 +20,30 @@ public class MapHandler {
 
     public MapHandler(){
         this.maps = new HashMap<String, GameMap>();
-        this.currentMap = new GameMap("initial",0,0); //debería arrancar en el mainmap
+        this.currentMap = new GameMap("initial",0,0); //should be main map or start in null
         this.size = 0;
     }
 
-    /*Multiple maps creator, podríamos crear una clase interna "MapInfo" que tenga
-    *todo lo que necesita este método para funcionar
+    /**
+    * Creates a GameMap from an array of ints and adds it to the collection
+    * of Maps so they can be later used.
+    * @param name: name of the map, which will be the key in the map
+    * @param height
+    * @param width
+    * @param mapData: array of ints that indicate what object has to be
+    * created in that specific position.
+    * @throws InvalidArgumentException if mapData is null or height or 
+    * width are 0
     */
-    //TODO:reparar, el mapa necesita que le pasen dimensiones, data y nombre
-    public void createMaps(ArrayList<int[][]> mapsdata, ArrayList<String> names){
-        for(int t = 0; t < mapsdata.size(); t++){
-            //createParticularMap(names.get(t),)
-        }
-        this.size += mapsdata.size();
-    }
-
-    //Create a particular map
     public void createParticularMap(String name, int height,
-                                    int width, int[][] mapdata){
+                                    int width, int[][] mapData) throws IllegalArgumentException{
+        if(mapData == null || name == null || height == 0 || width == 0){
+            throws new IllegalArgumentException();   
+        }
         GameMap map = new GameMap(name, height, width);
         for(int i = 0; i < map.getHeight(); i++){ //era al revez height y width?
             for(int j = 0; j < map.getWidth(); j++){
-                if(mapdata[i][j] == 1){ //XXX:ACA esta lo imperativo
+                if(mapData[i][j] == 1){ //XXX:ACA esta lo imperativo
                     map.add(new Position2D(i,j),
                             new Tile(false, false, new Wall()));
                 }else{
@@ -54,7 +54,6 @@ public class MapHandler {
         }
         this.size++;
         maps.put(name, map);
-
     }
 
     /**
@@ -62,25 +61,30 @@ public class MapHandler {
      * @param mapName
      * @throws MapsCollectionEmptyException if mapName isn't in the collection.
      */
-    public void removeMap(String mapName){
-        if(!isEmpty()){
-            maps.remove(mapName);
-        }else{
+    public void removeMap(String mapName) throws MapsCollectionEmptyException{
+        if(isEmpty()){
             throw new MapsCollectionEmptyException("There is no map loaded to remove!");
+        }
+        if(!maps.containsKey(mapName)){
+            throw new RuntimeException("No such map in collection"); // nueva exception?             
+        }else{
+              maps.remove(mapName);
         }
         this.size--;
     }
-
-    //Podría utilizar este metodo con un array y reemplazar el metodo de arriba
 
     /**
      * Same as removeMap only that removes an ArrayList of maps
      * @param mapNames
      */
-    public void removeMaps(ArrayList<String> mapNames){
+    public void removeMaps(ArrayList<String> mapNames) throws RuntimeException{ //cambiar
         if(!isEmpty()){
             for(int i = 0; i < mapNames.size(); i++){
-                maps.remove(mapNames.get(i));
+                if(!maps.containsKey(mapNames[i])){
+                    throw new RuntimeException("No such map in the Collection"); //crear nueva excepcion?
+                }else{
+                    maps.remove(mapNames.get(i));
+                }
             }
         }else{
             throw new MapsCollectionEmptyException("There is no map loaded to remove!");
@@ -88,20 +92,18 @@ public class MapHandler {
         this.size -= mapNames.size();
     }
 
-    /**
-     * Changes current working map
-     * @param mapName
-     */
-    //TODO Diferencia con setCurrentMap?
-    public void changeMap(String mapName){
-        this.currentMap = maps.get(mapName);
-    }
-
     public GameMap getCurrentMap(){
         return this.currentMap;
     }
 
-    public void setCurrentMap(String name){
+    /**
+     * Changes current working map
+     * @param mapName
+     */
+    public void setCurrentMap(String name) throws IllegalArgumentException{
+        if(name == null || !maps.containsKey(name)){
+            throw new IllegalArgumentException();   
+        }
         this.currentMap = this.maps.get(name);
     }
 
