fa421b142f1575c06308f26e4463c1de9126863b
tdallas <tdallas@itba.edu.ar>
2017-06-26 16:21:59 -0300

    Update AbilityFactory.java

--- core/src/model/abilities/AbilityFactory.java
+++ core/src/model/abilities/AbilityFactory.java
@@ -45,69 +45,68 @@ public class AbilityFactory{
     private static class BolaDeSabiduria extends Abilities{
 
         public BolaDeSabiduria(){
-            super("Bola de Sabiduria", 25, 15,2);
+            super("bolaDeSabiduria", 25, 15,2);
         }
 
     }
 
     private static class Induccion extends Abilities{
         public Induccion(){
-            super("Induccion", 15,12,2);
+            super("induccion", 15,12,2);
         }
     }
 
     private static class ReglaDeLaCadena extends Abilities{
         public ReglaDeLaCadena(){
-            super("Regla de la cadena", 20, 16 , 2);
+            super("reglaDeLaCadena", 20, 16 , 2);
         }
     }
 
     private static class AumentarVelocidadDeEscritura extends Abilities{
         public AumentarVelocidadDeEscritura(){
-            super("Aumentar velocidad de escritura", 30, 20, 2);
+            super("aumentarVelocidadDeEscritura", 30, 20, 2);
         }
     }
 
     private static class LaPapa extends Abilities{
         public LaPapa(){
-            super("LA PAPA", 15, 12, 2);
+            super("laPapa", 15, 12, 2);
         }
     }
 
     private static class NotaDeCursada extends Abilities{
         public NotaDeCursada(){
-            super("Cero va a ser su nota de cursada", 30, 20 ,2);
+            super("ceroNotaDeCursada", 30, 20 ,2);
         }
     }
 
     private static class TeoremaDe3Pizarrones extends Abilities{
         public TeoremaDe3Pizarrones(){
-            super ("Teorema de 3 pizarrrones", 17, 14 ,2);
+            super ("teoremaDe3Pizarrones", 17, 14 ,2);
         }
     }
 
     private static class CadaXHorasDeClase extends Abilities{
         public CadaXHorasDeClase(){
-            super ("Cada 24 horas de clase," +
-                    " tienen que estudiar 24 en casa mínimo", 20 ,15, 2);
+            super ("cadaXHorasDeClase", 20 ,15, 2);
         }
     }
 
     private static class HacerEjerciciosDeParcial extends Abilities{ //default hero
         public HacerEjerciciosDeParcial(){
-            super("Hacer ejercicios de parcial", 30, 20, 2);
+            super("hacerEjerciciosDeParcial", 30, 20, 2);
         }
     }
 
     private static class CentrarElEspiritu extends Abilities{ //mainChar
         public CentrarElEspiritu(){
-            super("Centrar el espiritu", 40, 20, 2);
+            super("centrarElEspiritu", 40, 20, 2);
         }
     }
 
     private static class MateEnClase extends Abilities{
         public MateEnClase(){
-            super("Tomar mate en clase", 25, 15, 2);
+            super("mateEnClase", 25, 15, 2);
         }
     }
 
@@ -119,7 +118,7 @@ public class AbilityFactory{
 
     private static class Eureka extends Abilities{
         public Eureka(){
-            super("Eureka!", 25, 15, 2);
+            super("eureka", 25, 15, 2);
         }
     }
 
